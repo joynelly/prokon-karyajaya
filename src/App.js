30ccf8c2e82e93f01275d75ee2af1216031da463
joynelly <nellyjoy@upi.edu>
2023-05-14 15:25:54 +0700

    add product and jasa page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './webpage/LandingPage/LandingPage';
 import AboutUsPage from './webpage/AboutUsPage/AboutUsPage';
+import ProductPage from './webpage/ProductPage/ProductPage'
 import DetailProductPage from './webpage/DetailProductPage/DetailProductPage';
+import JasaPage from './webpage/JasaPage/JasaPage';
+import DetailJasaPage from './webpage/DetailJasaPage/DetailProductPage';
 import AdminLoginPage from './webpage/AdminLoginPage/AdminLoginPage';
 import AdminLandingPage from './webpage/AdminLandingPage/AdminLandingPage';
 import AdminProductPage from './webpage/AdminProductPage/AdminProductPage';
@@ -16,7 +19,10 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage/>} />
           <Route path="/aboutus" element={<AboutUsPage/>}/>
+          <Route path="/product" element={<ProductPage/>}/>
           <Route path="/detailproduct" element={<DetailProductPage/>}/>
+          <Route path="/jasa" element={<JasaPage/>}/>
+          <Route path="/detailjasa" element={<DetailJasaPage/>}/>
           <Route path="/adminlogin" element={<AdminLoginPage/>}/>
           <Route path="/adminlandingpage" element={<AdminLandingPage/>}/>
           <Route path="/adminproductpage" element={<AdminProductPage/>}/>
