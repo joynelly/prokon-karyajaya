431c48801819df2c31d6088a8fc453b1bc8abf82
joynelly <nellyjoy@upi.edu>
2023-05-14 23:16:33 +0700

    Add Chart to Admin Dashboard

--- src/components/AdminBrandPageContent.js
+++ src/components/AdminBrandPageContent.js
@@ -62,18 +62,18 @@ const columns = [
         cell: (brand) => {
             return (
                 <Fragment>
-                    <button className="bg-light-blue-kj rounded-xl p-2 px-3 mx-1">
+                    <button className="bg-light-blue-kj rounded-xl p-2 px-3 m-1">
                         <svg class="h-6 w-6 text-black"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  
                             <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />  <circle cx="12" cy="12" r="3" />
                         </svg>
                     </button>  
-                    <button className="bg-yellow-400 rounded-xl p-2 px-3 mx-1">
+                    <button className="bg-yellow-400 rounded-xl p-2 px-3 m-1">
                         <svg class="h-6 w-6 text-black" width="24"  height="24"  viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  
                             <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />  
                             <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
                         </svg>
                     </button>  
-                    <button className="bg-red-400 rounded-xl p-2 px-3 mx-1" onClick={()=>{hanleDelete(brand.id, brand.brand_code)}}>
+                    <button className="bg-red-400 rounded-xl p-2 px-3 m-1" onClick={()=>{hanleDelete(brand.id, brand.brand_code)}}>
                         <svg class="h-6 w-6 text-black"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
                         </svg>
