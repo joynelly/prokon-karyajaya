f14c27a1a79086f914d6997a56f53b8de0b3999c
joynelly <nellyjoy@upi.edu>
2023-05-14 23:31:05 +0700

    fix line chart

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -8,11 +8,17 @@ const data = {
   labels: labels,
   datasets: [
     {
-      label: "Another Dataset",
+      label: "Produk",
       backgroundColor: "rgb(255, 99, 132)",
       borderColor: "rgb(255, 99, 132)",
-      data: [0, 10, 5, 2, 20, 30, 45],
+      data: [13, 10, 5, 2, 20, 30, 45],
     },
+    {
+        label: "Jasa",
+        backgroundColor: "rgb(95, 157, 247)",
+        borderColor: "rgb(95, 157, 247)",
+        data: [2, 5, 10, 15, 4, 8, 3],
+      },
   ],
 };
 
